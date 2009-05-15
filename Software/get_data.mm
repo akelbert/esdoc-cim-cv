2d1021ade7f7cec64f89d2311497de954abe13ed
charlotte <charlotte@4926d154-5f4e-0410-ae62-53d2c911a6ca>
2009-05-15 12:44:08 +0000

    we made this to deal with coupling
    git-svn-id: http://proj.badc.rl.ac.uk/svn/metafor/controlled_vocabularies/trunk@492 4926d154-5f4e-0410-ae62-53d2c911a6ca

---
+++ Software/get_data.mm
@@ -0,0 +1,21 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1242390662982" LINK="Ocean.mm" MODIFIED="1242391280748" TEXT="&lt;get data&gt;">
+<node CREATED="1242390722925" MODIFIED="1242390930138" POSITION="right" TEXT="file">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1242390726942" MODIFIED="1242391224276" POSITION="right" TEXT="other component(s)">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1242391016094" MODIFIED="1242391020431" TEXT="component 1"/>
+<node CREATED="1242391021562" MODIFIED="1242391024932" TEXT="component 2"/>
+<node CREATED="1242391034877" MODIFIED="1242391036374" TEXT="..."/>
+</node>
+<node CREATED="1242390712793" MODIFIED="1242390720343" POSITION="right" TEXT="at what frequency"/>
+<node CREATED="1242390749523" MODIFIED="1242390759474" POSITION="right" TEXT="interpolation method">
+<node CREATED="1242390799356" MODIFIED="1242390894648" TEXT="conservative remapping"/>
+<node CREATED="1242390858644" MODIFIED="1242390864946" TEXT="OASIS2 mosaic"/>
+<node CREATED="1242390807632" MODIFIED="1242390815868" TEXT="bilinear"/>
+<node CREATED="1242390816313" MODIFIED="1242390817249" TEXT="..."/>
+</node>
+</node>
+</map>
